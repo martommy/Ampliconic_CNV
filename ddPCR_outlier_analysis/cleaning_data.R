9b4dfd075f54448be7fc7d694b581aa99d983a70
Arslan Zaidi <arslanzaidipsu@gmail.com>
2017-12-07 15:40:15 -0500

    Update and rename R_script to cleaning_data.R

---
+++ ddPCR_outlier_analysis/cleaning_data.R
@@ -0,0 +1,70 @@
+
+#load REQUIRED packages
+require(plyr)
+require(reshape2)
+require(ggplot2)
+require(ape)
+require(ggtree)
+require(ggrepel)
+require(MASS)
+require(nlme)
+
+dat<-read.table('Table_S1A_ddPCR_outlier_analysis',header=T,sep="\t")
+
+#remove RPP30
+dat<-dat[-which(dat$Gene=="RPP30"),]
+
+###removing one replicate observation for each sample####
+
+#count number of nonmissing observations for each person
+
+dat$n<-apply(dat[,c(3:5)],1,function(x){length(which(is.na(x)=="FALSE"))})
+
+#calculate cv for each row before outlier removal
+dat$sd1<-apply(dat[,c(3:5)],1,sd,na.rm=T)
+dat$mean1<-apply(dat[,c(3:5)],1,mean,na.rm=T)
+dat$cv1<-dat$sd1/dat$mean1
+dat$median1<-apply(dat[,c(3:5)],1,median,na.rm=T)
+
+#plot cv for each gene before outlier removal - Figure S1A
+fig_s1a<-ggplot()+geom_boxplot(data=dat,aes(Gene,cv1),color="blue",outlier.shape=NA)+geom_point(data=dat,aes(Gene,cv1),position="jitter",alpha=0.7,color="blue")+theme_bw()+labs(x="Gene",y="Coefficient of Variation")+geom_hline(yintercept=median(dat$cv1,na.rm=T),color="red",linetype="dashed",size=1)+ylim(c(0,0.57))
+
+
+#determine which of the three replicates is the most distant from the other two. 
+dat$outlier<-NA
+for(i in 1:nrow(dat)){
+  if(dat$n[i]>2){
+    if(length(unique(as.character(dat[i,c('a','b','c')])))>1){
+      g1<-dat[i,c('a','b','c')]-dat[i,'median1']
+      distant.obs<-which(abs(g1)==max(abs(g1)))
+      if(length(distant.obs)>1){dat$outlier[i]<-sample(distant.obs,1)}else{dat$outlier[i]<-distant.obs}
+  }
+  }
+}
+
+#create new dataframe and replace outlier values with NA
+
+dat2<-dat[,c(1:6)]
+for(i in 1:nrow(dat2)){
+  if(is.na(dat$outlier[i])=="FALSE"){
+    outlier.index<-dat$outlier[i]
+    dat2[i,c('a','b','c')][outlier.index]<-NA
+  }
+}
+
+#calculate mean, sd and coefficient of variation per gene per individual after outlier removal
+dat2$n<-apply(dat2[,c(3:5)],1,function(x){length(which(is.na(x)=="FALSE"))})
+dat2$mean<-apply(dat2[,c('a','b','c')],1,mean,na.rm=T)
+dat2$sd<-apply(dat2[,c('a','b','c')],1,sd,na.rm=T)
+dat2$cv<-dat2$sd/dat2$mean
+
+
+
+#read table with haplogroup information for each ID
+haplo<-read.table('haplogroup_info_11202017.txt',sep="\t",header=T)
+
+#merge copy number with haplogroup info in one dataframe
+dat3<-merge(dat2,haplo,by="IID",sort=F)
+
+#plot coefficient of variation for each gene and red line with median cv across all genes after outlier removal - Figure S1B
+fig_s1b<-ggplot()+geom_boxplot(data=dat2,aes(Gene,cv),color="blue",outlier.shape=NA)+geom_point(data=dat2,aes(Gene,cv),position="jitter",alpha=0.7,color="blue")+theme_bw()+labs(x="Gene",y="Coefficient of Variation")+geom_hline(yintercept=median(dat2$cv,na.rm=T),color="red",linetype="dashed",size=1)
