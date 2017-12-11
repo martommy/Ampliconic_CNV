c7c9f4b71f5068fd36df8eaa88f8deba96159027
Arslan-Zaidi <Azaidi@github.com>
2017-12-11 11:57:32 -0500

    Reorganization_of_folders_and_data_tables
    - created new folders "Figures" and "Data_files"
    - made changes to cleanup script to load from, and write to, these folders
    - saved data files and some figures to these folders.
    - made changes to cleanup_script.R

--- ddPCR_data_cleanup/cleaning_data.R
+++ ddPCR_data_cleanup/cleaning_data.R
@@ -9,7 +9,7 @@ require(ggrepel)
 require(MASS)
 require(nlme)
 
-dat<-read.table('Table_S1A_ddPCR_outlier_analysis.txt',header=T,sep="\t")
+dat<-read.table('../Data_files/Table_S1A_ddPCR_outlier_analysis.txt',header=T,sep="\t")
 
 #remove RPP30
 #dat<-dat[-which(dat$Gene=="RPP30"),]
@@ -29,7 +29,7 @@ dat$median1<-apply(dat[,c(3:5)],1,median,na.rm=T)
 #plot cv for each gene before outlier removal - Figure S1A
 fig_s1a<-ggplot()+geom_boxplot(data=dat,aes(Gene,cv1),color="blue",outlier.shape=NA)+geom_point(data=dat,aes(Gene,cv1),position="jitter",alpha=0.7,color="blue")+theme_bw()+labs(x="Gene",y="Coefficient of Variation")+geom_hline(yintercept=median(dat$cv1,na.rm=T),color="red",linetype="dashed",size=1)+ylim(c(0,0.57))
 
-ggsave('Fig_S1A.pdf',fig_s1a,height=7,width=7)
+ggsave('../Figures/Fig_S1A.pdf',fig_s1a,height=7,width=7)
 
 #determine which of the three replicates is the most distant from the other two. 
 dat$outlier<-NA
@@ -62,7 +62,20 @@ dat2$cv<-dat2$sd/dat2$mean
 #plot coefficient of variation for each gene and red line with median cv across all genes after outlier removal - Figure S1B
 fig_s1b<-ggplot()+geom_boxplot(data=dat2,aes(Gene,cv),color="blue",outlier.shape=NA)+geom_point(data=dat2,aes(Gene,cv),position="jitter",alpha=0.7,color="blue")+theme_bw()+labs(x="Gene",y="Coefficient of Variation")+geom_hline(yintercept=median(dat2$cv,na.rm=T),color="red",linetype="dashed",size=1)
 
-ggsave('Fig_S1B.pdf',fig_s1b,height=7,width=7)
+ggsave('../Figures/Fig_S1B.pdf',fig_s1b,height=7,width=7)
+
+#add haplogroup information to copy number data
+#read table with haplogroup information for each ID
+haplo<-read.table('../Data_files/haplogroup_info_11202017.txt',sep="\t",header=T)
+
+#merge copy number with haplogroup info in one dataframe
+dat3<-merge(dat2,haplo,by="IID",sort=F)
+
+#pivot table so that ids are rows and columns are genes. average copy number across replicates
+dat4<-dcast(dat3,IID~Gene,value.var="mean")
+dat4$IID<-as.character(dat4$IID)
+dat4<-join(dat4,haplo,by="IID")
 
 #write cleaned data to file 
-write.table('ddpcr_outliers_removed.txt',sep="\t",col.names=T,row.names=F,quote=F)
+write.table(dat4,'../Data_files/ddpcr_outliers_removed.txt',sep="\t",col.names=T,row.names=F,quote=F)
+
